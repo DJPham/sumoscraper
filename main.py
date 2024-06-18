89b4617b12a44794266b9fcd870b665bb8101bd2
unknown <djpham1131@gmail.com>
2024-06-18 05:54:54 -0400

    Added JS scraping elements

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-# import the Scrapy library which will be used to scrape the data
+# scrapy will be used for HTML scraping
 import scrapy
 from scrapy.crawler import CrawlerProcess
+
+# using selenium for JavaScript scraping
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+# needed to help with selenium processes
+# automates the process instead of needing to manually set things up
+from webdriver_manager.chrome import ChromeDriverManager
+
+import time
 
 class SumoSpider(scrapy.Spider):
     name = 'sumo_spider'
     # the spider will start scraping from the home page
     start_urls = ['https://www.sumo.or.jp/En/']
+
+    # the *args and **kwargs allows a variable # of arguments, for flexibility
+    def __init__(self, *args, **kwargs):
+        # initialize the parent class, scrapy.Spider, super() returns a temp object to call its methods
+        super(SumoSpider, self).__init__(*args, **kwargs)
+
+        # initialize the Chrome Webdriver
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
     def parse(self, response):
         # it will try and find the "Find Rikishi" link
@@ -13,10 +32,29 @@ class SumoSpider(scrapy.Spider):
 
         # if the link is valid, return the results 
         if link:
-            yield response.follow(link, self.parse_rikishi)
+            # selenium's webdriver navigates to the full URL
+            # will also open up a Chrome window of the link
+            self.driver.get(response.urljoin(link))
 
-    def parse_rikishi(self, response):
-        self.log(response.text)
+            # give time for the JS to load
+            # could swap with WebDriverWait for more precise control
+            time.sleep(5)
+
+            # using selenium's functions to drive down to the specific code
+            # returns the names of every sumo wrestler in Makuuchi division
+            rikishi_elements = self.driver.find_elements(By.CSS_SELECTOR, 'table.mdTable3 tbody tr td.mdBr:first-of-type a')
+
+            # extract the values and put into an array
+            rikishi_names = [element.text for element in rikishi_elements]
+
+            for name in rikishi_names:
+                self.log(name)
+
+            yield {'rikishi_names': rikishi_names}
+
+    # used to properly close selenium whenever the spider is closed
+    def closed(self, reason):
+        self.driver.quit()
 
 process = CrawlerProcess()
 
